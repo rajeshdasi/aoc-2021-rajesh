d638a03834a46fdd7a6e21336946f03d50b92da5
rajeshdasi <rajeshdasi2719@gmail.com>
2021-12-01 17:30:51 +0530

    PartB commit of Day-1

--- src/Day01.kt
+++ src/Day01.kt
@@ -19,15 +19,22 @@ fun main() {
         return maxCount
     }
 
+    fun findMaxReadingPartB(data: List<Int>): Int {
+        val windowed: List<Int> = data.windowed(3, 1, true) { it.sum() }
+        return findMaxReadingsPartA(windowed)
+    }
+
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
     check(part1(testInput) == 1)
 
     val input = readInput("Day01")
     val resultOfPartA = findMaxReadingsPartA(input.map { it.toInt() })
+    val resultOfPartB = findMaxReadingPartB(input.map { it.toInt() })
     println(part1(input))
     println(part2(input))
     println(resultOfPartA)
+    println(resultOfPartB)
 }
 
 
