72ab2152c226bfa56e436edfeae807e4883b791a
Rajesh Dasi <rajesh.dasi@ericsson.com>
2021-12-01 11:25:32 +0100

    PartA commit of Day-1

--- src/Day01.kt
+++ src/Day01.kt
@@ -7,11 +7,27 @@ fun main() {
         return input.size
     }
 
+    fun findMaxReadingsPartA(data: List<Int>): Int {
+        var maxCount = 0
+        var index = 1
+        while (index < data.size) {
+            if (data[index] > data[index - 1]) {
+                maxCount++
+            }
+            index++
+        }
+        return maxCount
+    }
+
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
     check(part1(testInput) == 1)
 
     val input = readInput("Day01")
+    val resultOfPartA = findMaxReadingsPartA(input.map { it.toInt() })
     println(part1(input))
     println(part2(input))
+    println(resultOfPartA)
 }
+
+
